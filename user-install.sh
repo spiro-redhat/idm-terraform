55182c9f67c94daa39ce5e2d743364736e2d2021
Spiro Stathakis <spiro@redhat.com>
2025-01-22 14:53:33 +0000

    modified user-install.sh to example.com

--- user-install.sh
+++ user-install.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 # create a group for ansible users 
 sudo  bash -c 'cat << EOF  >> /etc/hosts
-10.0.1.11   rhel8-1.ipa.spidee.net
-10.0.1.12   rhel8-2.ipa.spidee.net
-10.0.1.13   rhel8-3.ipa.spidee.net
-10.0.1.21   rhel9-1.ipa.spidee.net
-10.0.1.22   rhel9-2.ipa.spidee.net
-10.0.1.23   rhel9-3.ipa.spidee.net
+10.0.1.11   rhel8-1.idm.example.com
+10.0.1.12   rhel8-2.idm.example.com
+10.0.1.13   rhel8-3.idm.example.com
+10.0.1.21   rhel9-1.idm.example.com
+10.0.1.22   rhel9-2.idm.example.com
+10.0.1.23   rhel9-3.idm.example.com
 EOF'
 
 
@@ -27,11 +27,11 @@ EOF
 cat << EOF > /home/ec2-user/.ssh/id_rsa
 -----BEGIN OPENSSH PRIVATE KEY-----
 -----END OPENSSH PRIVATE KEY-----
-EOF' 
+EOF 
 chmod -R 400 /home/ec2-user/.ssh/id_rsa
 
 cat << EOF > day2.sh
-    #! /usr/bin/env bash 
+#! /usr/bin/env bash 
 
 sudo subscription-manager register --username=
 sudo subscription-manager config --rhsm.manage_repos=1
@@ -40,17 +40,17 @@ sudo subscription-manager repos --enable=ansible-2.8-for-rhel-8-x86_64-rpms
 sudo dnf update -y
 sudo dnf install -y ansible ansible-freeipa 
 sudo subscription-manager repos --list-enabled
+EOF
 
-    cat >> EOF > inventory
-    [ipaserver]
-    rhel8-1.ipa.spidee.net
+cat >> EOF > inventory
+[ipaserver]
+rhel8-1.ipa.spidee.net
 
-    [ipaserver:vars]
-    ipaserver_domain=ipa.spidee.net
-    ipserver_realm=IPA.SPIDEE.NET
-    iapserver_setup_dns=yes
-    ipaserver_autoforwarders=yes
-    ipaadmin_password=password123
-    ipadm_password=password123
-    EOF
+[ipaserver:vars]
+ipaserver_domain=ipa.spidee.net
+ipserver_realm=IPA.SPIDEE.NET
+iapserver_setup_dns=yes
+ipaserver_autoforwarders=yes
+ipaadmin_password=password123
+ipadm_password=password123
 EOF
