b5ee7468118a43060b9351576dee2419e662f346
spiro-redhat <109510477+spiro-redhat@users.noreply.github.com>
2025-01-22 12:09:46 +0000

    Update user-install.sh

--- user-install.sh
+++ user-install.sh
@@ -8,7 +8,9 @@ sudo  bash -c 'cat << EOF  >> /etc/hosts
 10.0.1.22   rhel9-2
 10.0.1.23   rhel9-3
 EOF'
-
+export IP=$(ip -br addr show eth0 | awk '{ print $3 }' | sed -e 's/\/24//g')
+export HOSTNAME=$(grep $IP /etc/hosts  | awk '{ print $2 }')
+sudo hostnamectl hostname ${HOSTNAME} 
 
 
 mkdir -p /home/ec2-user/.ssh
@@ -24,6 +26,5 @@ cat << EOF > /home/ec2-user/.ssh/id_rsa
 -----BEGIN OPENSSH PRIVATE KEY-----
 -----END OPENSSH PRIVATE KEY-----
 EOF' 
-
-
 chmod -R 400 /home/ec2-user/.ssh/id_rsa
+
