972b74307be565474d707396bd023df00a862c8b
Spiro Stathakis <spiro@redhat.com>
2025-01-22 12:20:04 +0000

    bad whitespace in script

--- user-install.sh
+++ user-install.sh
@@ -8,6 +8,8 @@ sudo  bash -c 'cat << EOF  >> /etc/hosts
 10.0.1.22   rhel9-2.ipa.spidee.net
 10.0.1.23   rhel9-3.ipa.spidee.net
 EOF'
+
+
 export IP=$(ip -br addr show eth0 | awk '{ print $3 }' | sed -e 's/\/24//g')
 export HOSTNAME=$(grep $IP /etc/hosts  | awk '{ print $2 }')
 sudo hostnamectl hostname ${HOSTNAME} 
@@ -20,7 +22,7 @@ Host *
     User ec2-user 
     IdentityFile ~/.ssh/id_rsa
     StrictHostChecking No 
-EOF 
+EOF
 
 cat << EOF > /home/ec2-user/.ssh/id_rsa
 -----BEGIN OPENSSH PRIVATE KEY-----
