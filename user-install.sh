67e33bf3acc140503d389b46fe51d3466572583e
spiro-redhat <109510477+spiro-redhat@users.noreply.github.com>
2025-01-24 11:38:53 +0000

    Update user-install.sh

--- user-install.sh
+++ user-install.sh
@@ -1,12 +1,15 @@
 #! /usr/bin/env bash
 # create a group for ansible users 
 sudo  bash -c 'cat << EOF  >> /etc/hosts
-10.0.1.11   rhel8-1.idm.example.com
-10.0.1.12   rhel8-2.idm.example.com
-10.0.1.13   rhel8-3.idm.example.com
-10.0.1.21   rhel9-1.idm.example.com
-10.0.1.22   rhel9-2.idm.example.com
-10.0.1.23   rhel9-3.idm.example.com
+10.0.1.11   rhel7-1.ipa.spidee.net
+10.0.1.12   rhel7-2.ipa.spidee.net
+10.0.1.13   rhel7-3.ipa.spidee.net
+10.0.1.21   rhel8-1.ipa.spidee.net
+10.0.1.22   rhel8-2.ipa.spidee.net
+10.0.1.23   rhel8-3.ipa.spidee.net
+10.0.1.31   rhel9-1.ipa.spidee.net
+10.0.1.32   rhel9-2.ipa.spidee.net
+10.0.1.33   rhel9-3.ipa.spidee.net
 EOF'
 
 
@@ -30,25 +33,41 @@ cat << EOF > /home/ec2-user/.ssh/id_rsa
 EOF 
 chmod -R 400 /home/ec2-user/.ssh/id_rsa
 
+
+
 cat << EOF > /home/ec2-user/day2.sh
 #! /usr/bin/env bash
 
 sudo subscription-manager register --username=
 sudo subscription-manager config --rhsm.manage_repos=1
-# to become an ansible host
-sudo subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms
-sudo yum update -y
-sudo yum install -y ansible ansible-freeipa
+sudo subscription-manager repos --list-enabled
 EOF
 
 cat << EOF > /home/ec2-user/rhel8-idm.sh
 #! /usr/bin/env bash
 
-# install packaages
+# to become an ansible host
+sudo subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms
+sudo yum update -y
+sudo yum install -y ansible ansible-freeipa
+
 sudo yum -y module enable idm:DL1
 sudo yum -y distro-sync
 sudo yum -y module install idm:DL1/dns
 EOF
+
+
+cat << EOF > /home/ec2-user/rhel7-idm.sh
+#! /usr/bin/env bash
+yum install -y ipa-server ipa-server-dns 
+EOF
+
+cat << EOF > /home/ec2-user/rhel9-idm.sh
+#! /usr/bin/env bash
+dnf install -y ipa-server ipa-server-dns 
+# dnf install -y  ipa-server-trust-ad samba-client 
+EOF
+
 
 cat << EOF > /home/ec2-user/inventory
 [ipaserver]
